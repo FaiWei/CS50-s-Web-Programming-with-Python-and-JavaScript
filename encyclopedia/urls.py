165aeef403425d06ec08f767c517c77f30baabbd
FaiWei <zelenui.drakon@gmail.com>
2020-07-05 20:56:42 +0300

    3 req rdy, 4 more

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("search", views.search, name="search"),
+    path("wiki/<str:title>", views.wiki, name="wiki"),
+    path("add", views.add, name="add") 
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,41 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def wiki(request, title):
+    page_name = title
+    wiki_page = util.get_entry(title)
+    if wiki_page:
+        valueX = wiki_page
+    else:
+        valueX = False
+    return render(request, "encyclopedia/wiki.html", {
+        "value": valueX, "page_name": page_name
+    })  
+
+def add(request):
+    if request.method == "POST":
+        # request.POST - submited data 1.25 time in lecture
+        valueX = True
+    else:
+        valueX = False
+    return render(request, "encyclopedia/add.html", {
+        "value": valueX
+    })
+
+def search(request):
+    if request.method == "POST":
+        wiki_page = request.POST['q']
+        if util.get_entry(wiki_page):
+            return wiki(request, wiki_page)
+        else:
+            search_list = []
+            all_pages = util.list_entries()
+            for page in all_pages:
+                if (page.lower().find(wiki_page.lower()) + 1):
+                    search_list.append(page)
+            return render(request, "encyclopedia/search.html", {
+                "search_list": search_list, "wiki_page": wiki_page
+            })
+    else:
+        return index(request)
